692cbe48dd7b37de34bb4eb9ca5dd70e3caa1709
Snow <yh984980263@gmail.com>
2023-04-27 04:12:24 +1000

    add layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
+import { AppProps } from 'next/app'
+import Layout from '../components/AppLayout'
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
---
+++ src/pages/page1.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd';
+
+export default function Page1() {
+  return (
+    <>
+      <h1>This is page one</h1>
+      <Button type="primary">Example</Button>
+    </>
+  );
+}
