21691ea459963ce18822244edeb26f3bc7233315
Snow <yh984980263@gmail.com>
2023-04-28 20:13:52 +1000

    mock api

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,22 @@
 // export default Page
 import Head from "next/head";
 import styles from '@/styles/home.module.scss'
+import { Button } from "antd";
+import axios from "axios";
 
 export default function Home() {
+  const fetchData = async () => {
+    const response = await axios(`http://localhost:8000/api/v1/hello`, {
+      method: 'GET',
+    });
+    console.log(response.data.message)
+  };
+
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    fetchData();
+  };
+
   return (
     <>
       <Head>
@@ -10,6 +24,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className={styles.msg}>This is Homepage</h1>
+      <Button onClick={handleClick}>Click</Button>
     </>
   );
 }
