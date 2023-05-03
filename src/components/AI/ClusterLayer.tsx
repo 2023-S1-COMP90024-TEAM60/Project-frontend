07e28d61d3898c19532c632ff338a1b925b78546
David Lau <laukinfungdavid@gmail.com>
2023-05-03 22:40:01 +1000

    Removed example.ts testing code

--- src/components/AI/ClusterLayer.tsx
+++ src/components/AI/ClusterLayer.tsx
@@ -4,7 +4,6 @@ import {
   Source,
 } from 'react-map-gl';
 import { clusterCountLayerStyle, clusterLayerStyle, unclusteredPointLayerStyle } from './layers';
-import demoJson from "../../../public/example";
 
 const ClusterLayer = ({ heatMapData }: any) => {
   return(
@@ -13,7 +12,6 @@ const ClusterLayer = ({ heatMapData }: any) => {
       type="geojson"
       // data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
       data={heatMapData}
-      // data={demoJson}
       cluster={true}
       clusterMaxZoom={14}
       clusterRadius={50}
